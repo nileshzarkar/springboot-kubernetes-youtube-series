913b99825a134e24dbf83e5dab8a675758f716e6
Nilesh Zarkar <nileshzarkar@gmail.com>
2023-03-05 14:32:14 +0530

    Initial Commit

--- .github/bookmark-api/src/main/resources/db/migration/h2/V2__insert_sample_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO bookmarks (title, url, created_at) VALUES
-('How To Remove Docker Containers, Images, Volumes, and Networks', 'https://linuxize.com/post/how-to-remove-docker-images-containers-volumes-and-networks', CURRENT_TIMESTAMP()),
-('All You Need To Know About Unit Testing with Spring Boot', 'https://reflectoring.io/unit-testing-spring-boot', CURRENT_TIMESTAMP()),
-('Spring Boot Security Jwt Authentication', 'https://www.devglan.com/spring-security/spring-boot-jwt-auth', CURRENT_TIMESTAMP());
-
